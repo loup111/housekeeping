60971ef72251d14626a38d5f57157e499ef79c3e
loup111 <2596527874@qq.com>
2019-04-25 09:06:25 +0800

    项目第一次提交

---
+++ src/test/java/com/project/housekeeping/HousekeepingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.project.housekeeping;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HousekeepingApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
